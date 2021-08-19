cb253eb9aa9956eabd1e8d554de083a1a57247ef
ihan12389 <ihan1289@gmail.com>
2021-08-19 16:07:05 +0900

    루트 리졸버

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// 1. 'apollo-server'를 불러옵니다.
 const { ApolloServer } = require(`apollo-server`);
 
 const typeDefs = `
@@ -11,22 +10,30 @@ type Mutation {
 }
 `;
 
+// 1. 메모리에 사진을 저장할 때 사용할 데이터 타입
 var photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42,
+    // 2. 사진 배열의 길이를 반환합니다.
+    totalPhotos: () => photos.length,
+  },
+
+  // 3. Mutation & postPhoto 리졸버 함수
+  Mutation: {
+    postPhoto(parent, args) {
+      // 플레이스홀더인 parnet(Mutation)을 첫번재 인자로 넣어 리졸버로 전달한 두 번째 인자, 즉 뮤테이션 인자를 사용할 수 있도록 만듭니다.
+      photos.push(args);
+      return true;
+    },
   },
 };
 
-// 2. 서버 인스턴스를 새로 만듭니다.
-// 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달합니다.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// 4. 웹 서버를 구동하기 위해 listen 메서드를 호출합니다.
 server
   .listen()
   .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
