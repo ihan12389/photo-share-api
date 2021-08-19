699469065934fb451a3cc27c3dafcb0867e2a8c3
ihan12389 <ihan1289@gmail.com>
2021-08-19 21:03:44 +0900

    커스텀 스칼라

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { ApolloServer } = require(`apollo-server`);
+const { GraphQLScalarType } = require("graphql");
 
-// user 한명이 여러장의 사진에 태그도리 수 있고, photo 한장에는 여러명의 사용자가 태그될 수 있어서 다대다 관계가 생성됩니다.
-// taggedUsers 필드는 사용자 리스트를 반환하고 inPhotos 필드는 사용자가 태그된 사진 리스트를 반환합니다.
+// 스칼라 타입을 직접 만들 때는 타입 직렬화와 유효성 검사 방식을 고려해야 합니다.
+// DateTime 타입을 만들려고 한다면 유효한 DateTime에 대한 정의가 우선되어야 합니다.
+// typeDefs에 직접 만든 DateTime 스칼라 타입을 추가하여 Photo 타입의 created 필드에서 사용합니다. (created 필드는 사진이 게시된 날짜와 시간을 기록하는 용도입니다.)
 const typeDefs = `
 type User {
   githubLogin : ID!
@@ -19,7 +21,7 @@ enum PhotoCategory {
   GRAPHIC
 }
 
-
+scalar DateTime
 type Photo {
     id : ID!
     url: String!
@@ -28,6 +30,7 @@ type Photo {
     category : PhotoCategory!
     postedBy : User!
     taggedUsers:[User!]!
+    created:DateTime!
 }
 
 input PostPhotoInput {
@@ -38,7 +41,7 @@ input PostPhotoInput {
 
 type Query {
   totalPhotos : Int!
-  allPhotos: [Photo!]!
+  allPhotos(after: DateTime): [Photo!]!
 }
 
 type Mutation {
@@ -68,12 +71,14 @@ var photos = [
     description: "The heart chute is one of my favorite cutes",
     category: "ACTION",
     githubUser: "gPlake",
+    created: "3-28-1977",
   },
   {
     id: "2",
     name: "Enjoying the sunshine",
     category: "SELFIE",
     githubUser: "sSchmidt",
+    created: "1-2-1985",
   },
   {
     id: "3",
@@ -81,9 +86,9 @@ var photos = [
     description: "25 laps on gunbarrel today",
     category: "LANDSCAPE",
     githubUser: "sSchmidt",
+    created: "2018-04-15T19:09:57.308Z",
   },
 ];
-// 다대다 관계 테스트를 위해 샘플 태그 데이터가 담긴 배열 추가
 var tags = [
   { photoID: "1", userID: "gPlake" },
   { photoID: "2", userID: "sSchmidt" },
@@ -101,6 +106,7 @@ const resolvers = {
       var newPhoto = {
         id: _id++,
         ...args.input,
+        created: new Date(),
       };
       photos.push(newPhoto);
 
@@ -114,11 +120,8 @@ const resolvers = {
     },
     taggedUsers: (parent) =>
       tags
-        // 현재 사진에 대한 태그만 배열에 담아 반환합니다.
         .filter((tag) => tag.photoID === parent.id)
-        // 태그 배열을 userID 배열로 변환합니다.
         .map((tag) => tag.userID)
-        // userID 배열을 사용자 객체 배열로 변환합니다.
         .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
@@ -127,13 +130,25 @@ const resolvers = {
     },
     idPhotos: (parent) =>
       tags
-        // 현재 사용자에 대한 태그만 배열에 담아 변환합니다.
         .filter((tag) => tag.userID === parent.id)
-        // 태그 배열을 photoID 배열로 변환합니다.
         .map((tag) => tag.photoID)
-        // photoID배열을 사진 객체 배열로 변환합니다.
         .map((photoID) => photos.find((p) => p.id === photoID)),
   },
+  // GraphQLScalarType 객체로 커스텀 스칼라 타입에 대응하는 리졸버를 만들었습니다.
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value",
+    // after 인자는 리졸버로 보내지기 전에 자바스크립트 Date 객체로 변환됩니다.
+    // perseValue 함수로 쿼리와 함께 받아오는 문자열 값을 변환합니다. 여기서 반환하는 모든 값은 리졸버 함수의 인자로 보내지게 됩니다.
+    parseValue: (value) => new Date(value),
+    // 필드반환 값으로 날짜를 받으면 이를 ISO 형식 문자열 값으로 직렬화합니다.
+    // serialize 함수는 객체에서 필드 값을 가져다가 사용합니다.
+    serialize: (value) => new Date(value).toISOString(), //직렬화
+    // after 인자는 쿼리 변수로 넘기지 않고 코드에 곧장 써줍니다.
+    // 인자 값은 쿼리를 추상 구문 트리(AST)로 변환한 다음 파싱을 하기 전에 추출해야 합니다.
+    // parseLiteral 함수를 사용해 파싱 전의 인자 값을 쿼리에서 가져옵니다.
+    parseLiteral: (ast) => ast.value,
+  }),
 };
 
 const server = new ApolloServer({
